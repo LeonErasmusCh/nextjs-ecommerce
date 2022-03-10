e59b578708cac55ee94a789c442c9f540603cc04
Leon Erasmus <leondevcode@gmail.com>
2022-03-10 12:44:05 -0300

    navbar and styles for material ui

--- components/layouts.js
+++ components/layouts.js
@@ -1,20 +1,22 @@
 import Head from 'next/head';
 import React from 'react';
 import { AppBar, Toolbar, Typography, Container } from '@mui/material';
+import useStyles from '../utils/styles';
 
 export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Ecommerce</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>ecommerce</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>all rights reserved. Ecommerce</Typography>
       </footer>
     </div>
